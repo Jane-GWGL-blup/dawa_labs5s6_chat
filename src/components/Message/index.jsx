8a33b1bc379f0e78ca3582ea76b50bf6bec0b375
Nicole Caceres <janenieggcs@gmail.com>
2022-10-05 20:35:26 -0500

    Tercer commit: Se agregó colores a los mensajes de chat

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -10,6 +10,27 @@ const Message = ({ senderUser, messages }) => {
   console.log("entrando a message");
 }, []);
 //
+
+const c__message__contact__user = {
+  padding: "12px",
+  width: "300px",
+  height: "60px",
+  overflow: "auto",
+  backgroundColor: "rgb(139, 225, 32)",
+  borderRadius: "8px",
+  marginTop: "-10px",
+ }
+
+ const c__message__current__user= {
+  padding: "12px",
+  width: "300px",
+  height: "60px",
+  overflow: "auto",
+  backgroundColor: "rgb(114, 203, 226)",
+  borderRadius: "8px",
+  marginTop: "-10px",
+ }
+
  return (
    <>
    {messages.map((message, index) => (
@@ -30,7 +51,8 @@ const Message = ({ senderUser, messages }) => {
           ? localUser.name
           : senderUser.name}
       </p>
-      <p className="c__message">{message.message}</p>
+      <p style={senderUser.name === message.User.name ? c__message__contact__user: c__message__current__user }>
+        {message.message}</p>
     </div>
   </div>
    ))}
